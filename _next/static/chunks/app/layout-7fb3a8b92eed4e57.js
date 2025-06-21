d72ea0c36e338fd585ee7714c943e9e34617c3c4
NaveAi <NaveAi@users.noreply.github.com>
2025-06-21 20:25:40 +0000

    deploy: 48abeb30c6cd6198dc37a15ea60d1941392226b1

---
+++ _next/static/chunks/app/layout-7fb3a8b92eed4e57.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{7524:e=>{e.exports={style:{fontFamily:"'Heebo', 'Heebo Fallback'",fontStyle:"normal"},className:"__className_aadb56"}},9324:()=>{},9955:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,7524,23)),Promise.resolve().then(a.t.bind(a,9324,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[965,441,684,358],()=>s(9955)),_N_E=e.O()}]);
